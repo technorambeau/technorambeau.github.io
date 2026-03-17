67bed3f931a703e796807b17e01371fa07048448
technorambeau <s.rambeau@outlook.com>
2026-03-17 09:49:25 +0100

    Add files via upload

---
+++ _error-msg.php
@@ -0,0 +1,21 @@
+<?php
+$HttpStatus = "";
+if( isset($_SERVER["REDIRECT_STATUS"]) ) $HttpStatus = $_SERVER["REDIRECT_STATUS"];
+if( $HttpStatus == "" ) { @$HttpStatus = $_REQUEST["err"]; }
+$lang = "fr";
+if( isset($_SERVER["HTTP_ACCEPT_LANGUAGE"]) ) $lang = substr($_SERVER["HTTP_ACCEPT_LANGUAGE"], 0, 2);
+$redirecturl = "index.html";
+if( $HttpStatus == 404) {
+$redirecturl = "_message.html?==gP2lGZvwjP2lGZvwjPyJGP+InY84DcvwjbvlGdjVnc0NnbvNGIuVGIldWYw5Dc84jMo9CPg4jMoxjPiEmchBXL3RnI9M3chx2YgYXakxjPxg2L8IXdlJncF5TMoxjPiIXZ05WZjpjbnlGbh1Cd4VGdi0TZslHdzBidpRGP";}
+if( $HttpStatus == 500) {
+$redirecturl = "_message.html?=4jdpR2L84jdpR2L84jcixjPyJGP+A3L8I3byJXRgIXZ2JXZTBCbh5mclRnbJ5Dc84jMo9CPg4jMoxjPiEmchBXL3RnI9M3chx2YgYXakxjPxg2L8IXdlJncF5TMoxjPiIXZ05WZjpjbnlGbh1Cd4VGdi0TZslHdzBidpRGP";}
+if( headers_sent() ) {
+?>
+<html>
+<head>
+<?php echo('<meta http-equiv="Refresh" content="0; url=http://s.rambeau.free.fr/'.$redirecturl.'">'); ?>
+</head>
+<body><p style="text-align:center"><br><br><b><a href="http://s.rambeau.free.fr/index.html">Erreur</a></body></html>
+<?php
+} else exit( header( "Location: http://s.rambeau.free.fr/" . $redirecturl ) );
+?>
---
+++ groupe.php
@@ -0,0 +1,77 @@
+<?php
+$LOGIN_INFORMATION = array(
+  "6Rjrmko7!"
+);
+define( 'USE_USERNAME', false );
+define( 'LOGOUT_URL', 'http://s.rambeau.free.fr' );
+define( 'TIMEOUT_MINUTES', 60 );
+define( 'TIMEOUT_CHECK_ACTIVITY', true );
+$timeout = (TIMEOUT_MINUTES == 0 ? 0 : time() + TIMEOUT_MINUTES * 60);
+if( isset($_GET['logout']) ) {
+  setcookie( "verify", '', $timeout, '/' ); 
+  header( 'Location: ' . LOGOUT_URL );
+  exit();
+}
+if( !function_exists( 'showLoginPasswordProtect' ) ) {
+	function showLoginPasswordProtect( $error_msg ) {
+?>
+<!doctype html><html lang="fr"><head><META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE"><META HTTP-EQUIV="PRAGMA" CONTENT="NO-CACHE"><title>Accès restreint</title><meta charset="utf-8"><title>Groupe</title><meta name="author" content="s.rambeau"><meta name="rating" content="General"><meta name="robots" content="noindex,nofollow"><meta name="robots" content="noarchive"><meta name="generator" content="Lauyan TOWeb 8.1.3.813"><meta name="viewport" content="width=device-width, initial-scale=1.0"><link href="rss.xml" rel="alternate" type="application/rss+xml"><link href="_media/img/sq_icon/logotechno-3232.png" rel="shortcut icon" type="image/png"><link href="_media/img/thumb/logotechno.png" rel="apple-touch-icon"><meta name="msapplication-TileColor" content="#5835b8"><meta name="msapplication-TileImage" content="_media/img/thumb/logotechno.png"><link href="_scripts/bootstrap/css/bootstrap.min.css" rel="stylesheet"><link href="_frame/style.css" rel="stylesheet"><link rel="stylesheet" href="_scripts/bootstrap/css/font-awesome.min.css"><style>.alert a{color:#003399}.ta-left{text-align:left}.ta-center{text-align:center}.ta-justify{text-align:justify}.ta-right{text-align:right}.float-l{float:left}.float-r{float:right}</style><link href="_frame/print.css" rel="stylesheet" type="text/css" media="print"></head><body><div id="site"><div id="page"><header><div id="toolbar1" class="navbar"><div class="navbar-inner"><div class="container-fluid"><ul id="toolbar1_l" class="nav"><li><a id="logo" href="index.html"><span id="logo-lt">Techno </span><span id="logo-rt">Rambeau</span><br><span id="logo-sl">"L'échec est simplement l’opportunité de recommencer, cette fois de manière plus intelligente." – Henry Ford</span></a></li></ul><ul id="toolbar1_r" class="nav pull-right"><li><div id="sharebox"><a target="_blank" href="rss.xml" type="application/rss+xml" rel="noopener"><img style="width:24px;height:24px" src="_frame/tw-share-rss@2x.png" alt="rss"></a></div></li></ul></div></div></div><div id="toolbar2" class="navbar"><div class="navbar-inner"><div class="container-fluid"><ul id="toolbar2_r" class="nav pull-right"></ul><button type="button" class="btn btn-navbar" style="float:left" data-toggle="collapse" data-target=".nav-collapse"><span style="color:gray;text-shadow:none">Menu</span></button><div class="nav-collapse collapse"><ul id="toolbar2_l" class="nav"><li><ul id="mainmenu" class="nav"><li><a href="index.html">Notice</a></li><li><a href="presentation-de-la-technologie.html">C'est quoi la Techno ?</a></li><li><a href="contrat-et-sanctions.html">Ressources</a></li><li><a href="je-moccupe-quand-jai-fini.html">Je m'occupe quand j'ai fini !</a></li><li><a href="club-des-makers-coresponsables-fablab-creatif-vendredi-12h30-13h30.html">Club</a></li><li class="active"><a href="groupe.php">Prof</a></li><li><a href="archives.php">Archives</a></li><li><a href="sequence-01-test-de-survie-de-la-nasa.html">5°</a></li><li><a href="sequence-30-le-travail-de-groupe.html">3°</a></li><li><a href="revision-dnb.html">DNB</a></li></ul></li></ul></div></div></div></div></header><div id="content" class="container-fluid">
+	<div style="margin-top:20px; text-align:center">
+	<h1>Accès restreint</h1>
+	<form method="post" class="form-horizontal">
+	<p>Veuillez saisir le mot de passe pour accéder à cette page protégée</p>
+<?php 
+	if( $error_msg != "" ) 
+		echo '<div style="width:320px; margin:16px auto 0 auto"><div class="alert alert-error">'.$error_msg.'</div></div>';
+	if( USE_USERNAME ) 
+		echo 'Identifiant:<br><input class="input-medium" type="text" name="access_login" /><br>'; 
+?>
+	Mot de Passe :<br>
+    <input class="input-medium" type="password" name="access_password" /><p></p><input class="btn btn-primary" type="submit" name="Submit" value="Entrer" />
+	</form>
+<!--
+	<br>
+	<a style="font-size:10px; color: #B0B0B0; font-family: Verdana, Arial;" href="javascript:history.back()" title="">< Retour</a>
+-->	
+	</div>
+</div><footer><div id="footerfat" class="row-fluid"><div class="row-fluid"><div  id="footerfat_s1" class="span4 tw-para"><a href="https://ent.ecollege78.fr/auth/login?callback=https%3A%2F%2Fent.ecollege78.fr%2Ftimeline%2Ftimeline#/" target="_blank" rel="noopener nofollow" id="lnkb91b8eab">E-Collège NEO</a></div><div  id="footerfat_s2" class="span4 tw-para"><div style="text-align: center;"><a href="https://0780269u.index-education.net/pronote/?login=true" rel="nofollow" target="_blank" rel="noopener">Pronote</a></div></div><div  id="footerfat_s3" class="span4 tw-para"><div style="text-align: right;"><a href="me-contacter.html" target="_blank" rel="nofollow" id="lnk543f8bda">Me contacter</a></div></div></div></div><div id="toolbar3" class="navbar"><div class="navbar-inner"><div class="container-fluid"><ul id="toolbar3_r" class="nav pull-right"></ul></div></div></div><div id="footersmall" class="row-fluid"><div id="foot-sec1" class="span6 ">© Copyright Techno Rambeau. All Rights Reserved.</div><div id="foot-sec2" class="span6 "><div style="text-align: right; "><a href="_tos.html" id="lnkdfc5e39d">Terms &amp; Conditions</a></div></div></div></footer></div></div><script src="_scripts/jquery/jquery.min.js"></script><script src="_scripts/bootstrap/js/bootstrap.min.js"></script><script>function onChangeSiteLang(href){var i=location.href.indexOf("?");if(i>0)href+=location.href.substr(i);document.location.href=href;}</script><script>$(document).ready(function(){if(location.href.indexOf("?")>0&&location.href.indexOf("twtheme=no")>0){if(typeof twLzyLoad!=="undefined"){window.addEventListener("load",twLzyLoad);window.addEventListener("scroll",twLzyLoad);window.addEventListener("resize",twLzyLoad);}$("#toolbar1,#toolbar2,#toolbar3,#footersmall,#footerfat").hide();var idbmk=location.href;idbmk=idbmk.substring(idbmk.lastIndexOf("#")+1,idbmk.lastIndexOf("?"));if(idbmk!="")$("html,body").animate({scrollTop:$("#"+idbmk).offset().top},0);}$("#site").prepend("<a href='javascript:void(0)' class='toTop' title='Haut de page'><i class='fa fa-angle-double-up fa-3x toTopLink'></i></a>");var offset=220;var duration=500;$(window).scroll(function(){if($(this).scrollTop()>offset){$(".toTop").fadeIn(duration);}else{$(".toTop").fadeOut(duration);}});$(".toTop").click(function(event){event.preventDefault();$("html, body").animate({scrollTop:0},duration);return(false);});if(typeof onTOWebPageLoaded=="function")onTOWebPageLoaded();});</script></body></html>
+<?php
+	  die();
+	}
+}
+if (isset($_POST['access_password'])) {
+  $login = isset($_POST['access_login']) ? $_POST['access_login'] : '';
+  $pass = $_POST['access_password'];
+  if (!USE_USERNAME && !in_array($pass, $LOGIN_INFORMATION)
+  || (USE_USERNAME && ( !array_key_exists($login, $LOGIN_INFORMATION) || $LOGIN_INFORMATION[$login] != $pass ) ) 
+  ) {
+    showLoginPasswordProtect("Mot de passe incorrect. Accès refusé.");
+  }
+  else {
+    setcookie("verify", md5($login.'%'.$pass), $timeout, '/');
+    unset($_POST['access_login']);
+    unset($_POST['access_password']);
+    unset($_POST['Submit']);
+  }
+}
+else {
+  if (!isset($_COOKIE['verify'])) {
+    showLoginPasswordProtect("");
+  }
+  $found = false;
+  foreach($LOGIN_INFORMATION as $key=>$val) {
+    $lp = (USE_USERNAME ? $key : '') .'%'.$val;
+    if ($_COOKIE['verify'] == md5($lp)) {
+      $found = true;
+      if (TIMEOUT_CHECK_ACTIVITY) {
+        setcookie("verify", md5($lp), $timeout, '/');
+      }
+      break;
+    }
+  }
+  if (!$found) {
+    showLoginPasswordProtect("");
+  }
+}
+?>
+<!doctype html><html lang="fr"><head><meta charset="utf-8"><title>Groupe</title><meta name="author" content="s.rambeau"><meta name="rating" content="General"><meta name="robots" content="noindex,nofollow"><meta name="robots" content="noarchive"><meta name="generator" content="Lauyan TOWeb 8.1.3.813"><meta name="viewport" content="width=device-width, initial-scale=1.0"><link href="rss.xml" rel="alternate" type="application/rss+xml"><link href="_media/img/sq_icon/logotechno-3232.png" rel="shortcut icon" type="image/png"><link href="_media/img/thumb/logotechno.png" rel="apple-touch-icon"><meta name="msapplication-TileColor" content="#5835b8"><meta name="msapplication-TileImage" content="_media/img/thumb/logotechno.png"><link href="_scripts/bootstrap/css/bootstrap.min.css" rel="stylesheet"><link href="_frame/style.css" rel="stylesheet"><link rel="stylesheet" href="_scripts/bootstrap/css/font-awesome.min.css"><style>.alert a{color:#003399}.ta-left{text-align:left}.ta-center{text-align:center}.ta-justify{text-align:justify}.ta-right{text-align:right}.float-l{float:left}.float-r{float:right}</style><link href="_frame/print.css" rel="stylesheet" type="text/css" media="print"></head><body><div id="site"><div id="page"><header><div id="toolbar1" class="navbar"><div class="navbar-inner"><div class="container-fluid"><ul id="toolbar1_l" class="nav"><li><a id="logo" href="index.html"><span id="logo-lt">Techno </span><span id="logo-rt">Rambeau</span><br><span id="logo-sl">"L'échec est simplement l’opportunité de recommencer, cette fois de manière plus intelligente." – Henry Ford</span></a></li></ul><ul id="toolbar1_r" class="nav pull-right"><li><div id="sharebox"><a target="_blank" href="rss.xml" type="application/rss+xml" rel="noopener"><img style="width:24px;height:24px" src="_frame/tw-share-rss@2x.png" alt="rss"></a></div></li></ul></div></div></div><div id="toolbar2" class="navbar"><div class="navbar-inner"><div class="container-fluid"><ul id="toolbar2_r" class="nav pull-right"></ul><button type="button" class="btn btn-navbar" style="float:left" data-toggle="collapse" data-target=".nav-collapse"><span style="color:gray;text-shadow:none">Menu</span></button><div class="nav-collapse collapse"><ul id="toolbar2_l" class="nav"><li><ul id="mainmenu" class="nav"><li><a href="index.html">Notice</a></li><li><a href="presentation-de-la-technologie.html">C'est quoi la Techno ?</a></li><li><a href="contrat-et-sanctions.html">Ressources</a></li><li><a href="je-moccupe-quand-jai-fini.html">Je m'occupe quand j'ai fini !</a></li><li><a href="club-des-makers-coresponsables-fablab-creatif-vendredi-12h30-13h30.html">Club</a></li><li class="active"><a href="groupe.php">Prof</a></li><li><a href="archives.php">Archives</a></li><li><a href="sequence-01-test-de-survie-de-la-nasa.html">5°</a></li><li><a href="sequence-30-le-travail-de-groupe.html">3°</a></li><li><a href="revision-dnb.html">DNB</a></li></ul></li></ul></div></div></div></div></header><div id="content" class="container-fluid"><h1>Groupe</h1><div id="topic" class="row-fluid"><div id="topic-inner"><div id="top-content" class="span12"><div class="row-fluid"><ul class="thumbnails"><li class="span12"><div class="tw-para tw-parallax" id="Z9jnANz1"><a href="corrections.html"><h2>Corrections</h2></a><p>Correction activité 1 : s10-activite1-testnavy-prof.pdf </p><p style="text-align:right;"><a class="btn btn-primary" href="corrections.html">Suite...</a></p></div></li></ul></div><div class="row-fluid"><ul class="thumbnails"><li class="span12"><div class="tw-para tw-parallax" id="P0AAb6xD"><a href="dnb-blanc-2025-ecouteursbluetooth.html"><h2>DNB Blanc : 2025 - EcouteursBluetooth </h2></a><p>OST11 - Collecter, trier et analyser des donnéesOST1b : La fonction technique, le principe technique OST1c : La famille et la lignée d’OST OST33 - Choisir un OST parmi plusieurs propositions en vue de répondre à un besoin. OST3c - Les impacts environnementaux (indicateurs : air, eau, sol et santé) OST3e - Les critères de choix : la qualité,...</p><p style="text-align:right;"><a class="btn btn-primary" href="dnb-blanc-2025-ecouteursbluetooth.html">Suite...</a></p></div></li></ul></div><div class="row-fluid"><ul class="thumbnails"><li class="span12"><div class="tw-para tw-parallax" id="6FH7ZR4W"><a href="ressources-professeur.html"><h2>Ressource : Prof</h2></a>Ressources à destination du professeur</div></li></ul></div></div></div></div><footer><div id="footerfat" class="row-fluid"><div class="row-fluid"><div  id="footerfat_s1" class="span4 tw-para"><a href="https://ent.ecollege78.fr/auth/login?callback=https%3A%2F%2Fent.ecollege78.fr%2Ftimeline%2Ftimeline#/" target="_blank" rel="noopener nofollow" id="lnkb91b8eab">E-Collège NEO</a></div><div  id="footerfat_s2" class="span4 tw-para"><div style="text-align: center;"><a href="https://0780269u.index-education.net/pronote/?login=true" rel="nofollow" target="_blank" rel="noopener">Pronote</a></div></div><div  id="footerfat_s3" class="span4 tw-para"><div style="text-align: right;"><a href="me-contacter.html" target="_blank" rel="nofollow" id="lnk543f8bda">Me contacter</a></div></div></div></div><div id="toolbar3" class="navbar"><div class="navbar-inner"><div class="container-fluid"><ul id="toolbar3_r" class="nav pull-right"></ul></div></div></div><div id="footersmall" class="row-fluid"><div id="foot-sec1" class="span6 ">© Copyright Techno Rambeau. All Rights Reserved.</div><div id="foot-sec2" class="span6 "><div style="text-align: right; "><a href="_tos.html" id="lnkdfc5e39d">Terms &amp; Conditions</a></div></div></div></footer></div></div><script src="_scripts/jquery/jquery.min.js"></script><script src="_scripts/bootstrap/js/bootstrap.min.js"></script><script>function onChangeSiteLang(href){var i=location.href.indexOf("?");if(i>0)href+=location.href.substr(i);document.location.href=href;}</script><script>$(document).ready(function(){if(location.href.indexOf("?")>0&&location.href.indexOf("twtheme=no")>0){if(typeof twLzyLoad!=="undefined"){window.addEventListener("load",twLzyLoad);window.addEventListener("scroll",twLzyLoad);window.addEventListener("resize",twLzyLoad);}$("#toolbar1,#toolbar2,#toolbar3,#footersmall,#footerfat").hide();var idbmk=location.href;idbmk=idbmk.substring(idbmk.lastIndexOf("#")+1,idbmk.lastIndexOf("?"));if(idbmk!="")$("html,body").animate({scrollTop:$("#"+idbmk).offset().top},0);}$("#site").prepend("<a href='javascript:void(0)' class='toTop' title='Haut de page'><i class='fa fa-angle-double-up fa-3x toTopLink'></i></a>");var offset=220;var duration=500;$(window).scroll(function(){if($(this).scrollTop()>offset){$(".toTop").fadeIn(duration);}else{$(".toTop").fadeOut(duration);}});$(".toTop").click(function(event){event.preventDefault();$("html, body").animate({scrollTop:0},duration);return(false);});if(typeof onTOWebPageLoaded=="function")onTOWebPageLoaded();});</script></body></html>
